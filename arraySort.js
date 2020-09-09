d49bf8d9d191c8cc06910e4829faae93aecb32d5
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2020-09-10 00:46:45 +0100

    fix

--- arraySort.js
+++ arraySort.js
@@ -1,3 +1,3 @@
-const frameworks = ["Vue", "Angular", " Gatsby", "Next"]
-frameworks.sort()
-frameworks.reverse()
+const frameworks = ["Vue", "Angular", " Gatsby", "Next"];
+frameworks.sort(); //takes all arguments and perform the sort operation
+frameworks.reverse(); // reversing all

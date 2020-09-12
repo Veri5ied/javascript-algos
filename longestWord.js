36495be1f33982f5dc630d3373177fdc4ac1c2ed
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2020-09-12 08:18:02 +0100

    Max word

--- longestWord.js
+++ longestWord.js
@@ -1,1 +1,17 @@
 //an algo to find the longest word in a sentence
+const sentence = "The quick brown fox jumped over the roof";
+
+const longestWord = (sentence) => {
+  let splittedSentence = sentence.split(" ");
+  let maxLength = 0;
+  let maxWord = "";
+
+  splittedSentence.map(word => {
+    if (word.length > maxLength) {
+      maxLength = word.length;
+      maxWord = word;
+    }
+  });
+
+  return maxWord;
+}

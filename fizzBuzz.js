5d0e373d3c13f01bfa54e1877df5fe6ae29dd3c1
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2020-09-12 08:02:19 +0100

    Algo

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,1 +1,12 @@
 //fizzbuzz algorithm
+for (let theNumber=0; theNumber <= 100; theNumber++) {
+  if (theNumber % 15 == 0){
+    console.log("Fizz Buzz")
+  }else if (theNumber % 5 == 0){
+    console.log("Buzz")
+  }else if (theNumber % 3 == 0){
+    console.log("Fizz")
+  }else {
+    console.log(theNumber)
+  }
+}
